0bf2f50665afd0da986699bed52bae0a82abcaa4
jam1nb3n <benewman2002@gmail.com>
2020-11-10 11:54:24 +0000

    edit?

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-#!/usr/bin/env nodejs
-const https = require("https");
-const fs = require("fs");
-
-const optionsD = {
-    key: fs.readFileSync("/etc/letsencrypt/live/bnewman.dev/privkey.pem"),
-    cert: fs.readFileSync("/etc/letsencrypt/live/bnewman.dev/fullchain.pem")
-};
-
-const options = {
-    
-}
-
 const express = require("express");
-
-
-const port = 443;
+const port = 3000;
 const app = express();
 
 	
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
-const server = https.createServer(options, app);
-server.listen(port, () => {
-    console.log("Server Online");
-})
+app.listen(port, () => console.log(`Listening on port ${port}`))
