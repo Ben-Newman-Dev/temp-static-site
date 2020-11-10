7d673c41d8c9f5119828c5249d354e89d85f78fb
jam1nb3n <benewman2002@gmail.com>
2020-11-10 17:46:10 +0000

    1

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const port = 3000;
 const app = express();
 const favicon = require('serve-favicon');
+const { default: HomePage } = require("./pages");
 
 app.use("./public/favicon.ico");
-app.use(express.static('./public'));
+app.use('/',express.static('./public'));
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
