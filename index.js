c61e9c634f5e8e60f37ddb81826f24c907ea5bb4
jam1nb3n <benewman2002@gmail.com>
2020-11-11 02:27:53 +0000

    tirdboi

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const port = 3000;
 const app = express();
-const favicon = require('serve-favicon');
-const { default: HomePage } = require("./pages");
+// const favicon = require('serve-favicon');
+// const { default: HomePage } = require("./pages");
 
-app.use("./public/favicon.ico");
+// app.use("./public/favicon.ico");
 app.use('/',express.static('./public'));
 
 
